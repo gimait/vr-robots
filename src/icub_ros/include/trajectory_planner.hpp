98ba68f62afb22c8780b33445f57ad9e3f132ad9
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-07-14 11:07:32 +1000

    Fixed bad interpolation start.

--- src/icub_ros/include/trajectory_planner.hpp
+++ src/icub_ros/include/trajectory_planner.hpp
@@ -3,6 +3,7 @@
 #include <moveit_msgs/RobotState.h>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <tf2_eigen/tf2_eigen.h>
 
 #include "geometry.hpp"
 #include "icub_ros/MoveService.h"
@@ -16,13 +17,15 @@ public:
   void startService();
   ros::V_string getJointsForLinks(ros::V_string links);
   ros::V_string getLinksForJoints(ros::V_string joints);
+  geometry_msgs::Pose calculateTargetPosition(geometry_msgs::Pose target, Eigen::Isometry3d origin);
   geometry_msgs::Pose calculateTargetPosition(geometry_msgs::Pose target, geometry_msgs::Pose origin);
 
 protected:
   std::string m_move_group;
   ros::ServiceServer m_plan_trajectory_service;
   moveit::planning_interface::MoveGroupInterfacePtr m_move_group_interface;
-
+  moveit::core::RobotModelConstPtr m_kinematic_model;
+  moveit::core::RobotStatePtr m_kinematic_state;
   ros::V_string convertBetweenLists(ros::V_string s_list, ros::V_string from_list, ros::V_string to_list);
 };
 
@@ -31,6 +34,8 @@ TrajectoryPlanner::TrajectoryPlanner(std::string move_group)
   m_move_group = move_group;
   m_move_group_interface =
       moveit::planning_interface::MoveGroupInterfacePtr(new moveit::planning_interface::MoveGroupInterface(move_group));
+  m_kinematic_model = m_move_group_interface->getRobotModel();
+  m_kinematic_state = moveit::core::RobotStatePtr(new moveit::core::RobotState(m_kinematic_model));
 }
 
 void TrajectoryPlanner::startService()
@@ -41,14 +46,14 @@ void TrajectoryPlanner::startService()
 
 ros::V_string TrajectoryPlanner::getJointsForLinks(ros::V_string links)
 {
-  return this->convertBetweenLists(links, m_move_group_interface->getRobotModel()->getLinkModelNames(),
-                                   m_move_group_interface->getRobotModel()->getJointModelNames());
+  return convertBetweenLists(links, m_move_group_interface->getRobotModel()->getLinkModelNames(),
+                             m_move_group_interface->getRobotModel()->getJointModelNames());
 }
 
 ros::V_string TrajectoryPlanner::getLinksForJoints(ros::V_string joints)
 {
-  return this->convertBetweenLists(joints, m_move_group_interface->getRobotModel()->getJointModelNames(),
-                                   m_move_group_interface->getRobotModel()->getLinkModelNames());
+  return convertBetweenLists(joints, m_move_group_interface->getRobotModel()->getJointModelNames(),
+                             m_move_group_interface->getRobotModel()->getLinkModelNames());
 }
 
 ros::V_string TrajectoryPlanner::convertBetweenLists(ros::V_string s_list, ros::V_string from_list,
@@ -67,26 +72,30 @@ ros::V_string TrajectoryPlanner::convertBetweenLists(ros::V_string s_list, ros::
   return output;
 }
 
+geometry_msgs::Pose TrajectoryPlanner::calculateTargetPosition(geometry_msgs::Pose target, Eigen::Isometry3d origin)
+{
+  return calculateTargetPosition(target, tf2::toMsg(origin));
+}
+
 geometry_msgs::Pose TrajectoryPlanner::calculateTargetPosition(geometry_msgs::Pose target, geometry_msgs::Pose origin)
 {
   const robot_state::JointModelGroup *joint_model_group =
-      this->m_move_group_interface->getRobotModel()->getJointModelGroup(m_move_group);
+      m_move_group_interface->getRobotModel()->getJointModelGroup(m_move_group);
 
   // Return if the target is already reachable.
-  if (this->m_move_group_interface->getCurrentState()->setFromIK(joint_model_group, target))
+  if (m_move_group_interface->getCurrentState()->setFromIK(joint_model_group, target))
   {
     return target;
   }
 
   // Generate a path by creating a set of angles that move incrementally towards the goal (in a line) from the current
   // pose.
-
   std::vector<geometry_msgs::Pose> possible = calculateLinearPath(target, origin, 100);
 
   // Check backwards all calculated poses until we find a reachable one.
   for (auto pose = possible.rbegin(); pose != possible.rend(); ++pose)
   {
-    if (this->m_move_group_interface->getCurrentState()->setFromIK(joint_model_group, *pose))
+    if (m_move_group_interface->getCurrentState()->setFromIK(joint_model_group, *pose))
     {
       return *pose;
     }
@@ -98,34 +107,34 @@ geometry_msgs::Pose TrajectoryPlanner::calculateTargetPosition(geometry_msgs::Po
 
 bool TrajectoryPlanner::planTrajectoryService(icub_ros::MoveService::Request &req, icub_ros::MoveService::Response &res)
 {
-  sensor_msgs::JointState js;
-  js.name = this->getJointsForLinks(req.link_names);
-  js.position = req.joint_positions;
   moveit_msgs::RobotState rs;
-  rs.joint_state = js;
+  rs.joint_state.name = getJointsForLinks(req.link_names);
+  rs.joint_state.position = req.joint_positions;
+
+  const moveit::core::JointModelGroup *joint_model_group = m_kinematic_model->getJointModelGroup(m_move_group);
+  m_kinematic_state->setJointGroupPositions(joint_model_group, req.joint_positions);
   m_move_group_interface->setStartState(rs);
 
-  geometry_msgs::Pose current_pose = this->m_move_group_interface->getCurrentPose().pose;
+  geometry_msgs::Pose target = calculateTargetPosition(
+      req.target_pose, m_kinematic_state->getGlobalLinkTransform(m_move_group_interface->getEndEffectorLink()));
 
-  geometry_msgs::Pose target = this->calculateTargetPosition(req.target_pose, current_pose);
-
-  std::vector<double> ik_seed_state = m_move_group_interface->getCurrentJointValues();
-  std::vector<double> solution;
   moveit_msgs::MoveItErrorCodes error_code;
   kinematics::KinematicsQueryOptions options = kinematics::KinematicsQueryOptions();
-
-  // Print names of used joints.
-  std::copy(js.name.begin(), js.name.end(), std::ostream_iterator<std::string>(std::cout, ", "));
 
   m_move_group_interface->setPoseTarget(target);
 
   moveit::planning_interface::MoveGroupInterface::Plan new_plan;
   bool success = (m_move_group_interface->plan(new_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
 
-  new_plan.trajectory_.joint_trajectory.joint_names =
-      this->getLinksForJoints(new_plan.trajectory_.joint_trajectory.joint_names);
-
-  res.trajectories.push_back(new_plan.trajectory_);
-
-  return true;
+  if (success)
+  {
+    new_plan.trajectory_.joint_trajectory.joint_names =
+        getLinksForJoints(new_plan.trajectory_.joint_trajectory.joint_names);
+    res.trajectories.push_back(new_plan.trajectory_);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
